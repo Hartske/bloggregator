95ceb01dbf8ee2d79d66de150c3428ebde1ed09e
Hartske <keatoru@gmail.com>
2025-06-28 13:07:20 -0700

    added command to edit config

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+
+	"github.com/Hartske/bloggregator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
-	home, err := os.UserHomeDir()
+	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println("It broke")
+		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("%s", home)
+
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		allCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
